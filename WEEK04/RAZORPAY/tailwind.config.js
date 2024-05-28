d69b6b8bc59d0d86b5a5a97ae398859ec11c4496
iamahmarfaraz <ahmarfaraz9@gmail.com>
2024-05-28 16:27:50 +0530

    Razorpay Clone

--- WEEK04/RAZORPAY/tailwind.config.js
+++ WEEK04/RAZORPAY/tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
       fontFamily: {
         mullish: ["Mulish", "sans-serif"],
       },
+      boxShadow: {
+        'black': '0 4px 6px rgba(0, 0, 0, 1)',
+        'black-md': '0 6px 10px rgba(0, 0, 0, 1)',
+        'black-lg': '0 10px 20px rgba(0, 0, 0, 1)',
+      },
       colors: {
         deepBlue: "#02042a",
         lightBlue: "#2b84ea",
